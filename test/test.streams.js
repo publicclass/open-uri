1f562d581f3ec17544e04d15a35feb7c309eaba4
Robert Sköld <robert@publicclass.se>
2012-08-13 14:55:54 +0200

    Removed old tests.

--- test/test.streams.js
+++
@@ -1,6 +0,0 @@
-var open = require('../')
-
-// Stream from HTTP to both FTP and File at once...
-var xyz = open('https://encrypted.google.com/search?q=open+uri')
-// xyz.pipe(open('ftp://abc'))
-xyz.pipe(open('file:///tmp/enc-google-'+Date.now()+'.html'))
